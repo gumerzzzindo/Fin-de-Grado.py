4a684160979e760a179ff150253a344b6c601b2c
gumerzzzindo <aitor.santos@maristak.net>
2024-03-20 13:45:47 +0100

    ah

--- pass_gen.py
+++ pass_gen.py
@@ -4,20 +4,22 @@ import random
 import secrets
 import string
 
+# Definir los conjuntos de caracteres disponibles para la contraseña
 letras = string.ascii_letters
 numeros = string.digits
 especial = string.punctuation
 seleccion = letras + numeros + especial
 
 long = int(input("Inserta la longitud de caracteres deseada: "))
-
+# Condicion de si es menor que 8 de un aviso de que es recomendable mayor que 8
 if long < 8:
     print("Es recomendable que sea mayor o igual a 8 caracteres.")
 
 else:
     contra = ''
-for i in range(long):
-    contra+= ''.join(secrets.choice(selecion))
+    for i in range(long):
+        contra += ''.join(secrets.choice(seleccion))
     # Imprimios resultado
-print(contra)
+
+print("Contraseña generada:",contra)
     
